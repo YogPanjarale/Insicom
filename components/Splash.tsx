651b4ee8a3a3f7e82a6df4a4388970277f06c034
Yog <yog.panjarale@gmail.com>
2022-01-15 20:17:57 +0530

    font problem fixed

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -3,7 +3,7 @@ export default function SplashScreen({ quote }: { quote?: string }) {
 		<main className=" w-full h-screen px-10 text-center bg-primary-light font-sans-serif flex flex-col items-center">
 			<div className="h-full flex flex-col justify-center">
 				{!!quote ? (
-					<h3 className="transform text-3xl w-full text-center font-blinker font-semibold">
+					<h3 className="transform text-4xl w-full text-center font-blinker font-semibold">
 						{quote}
 					</h3>
 				) : (
