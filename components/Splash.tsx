7f7fac1df432d30746492f3a38987c9c8f68b8d8
Yog <yog.panjarale@gmail.com>
2022-01-15 20:13:07 +0530

    added quote animation

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -3,7 +3,9 @@ export default function SplashScreen({ quote }: { quote?: string }) {
 		<main className=" w-full h-screen px-10 text-center bg-primary-light font-sans-serif flex flex-col items-center">
 			<div className="h-full flex flex-col justify-center">
 				{!!quote ? (
-					<h3 className="text-3xl w-full text-center font-blinker font-semibold">{quote}</h3>
+					<h3 className="transform text-3xl w-full text-center font-blinker font-semibold">
+						{quote}
+					</h3>
 				) : (
 					<img
 						src="/assets/logo.svg"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
 import SplashScreen from "../components/Splash";
 
-export  default function Index() {
-  return (
-    <div className=" min-h-screen">
-
-      <SplashScreen quote="MCD sells 1 billion burgers every year"/>
-    </div>
-  );
+export default function Index() {
+	const [quote, setQuote] = useState("");
+	useEffect(() => {
+		setTimeout(() => {
+			setQuote("   ");
+			setTimeout(() => {
+				setQuote("MCD sells 1 billion burgers every year");
+			}, 500);
+		}, 1500);
+	}, []);
+	return (
+		<div className=" min-h-screen">
+			<Head>
+				<title>Insicom</title>
+				<link rel="icon" href="/assets/logo.svg" />
+			</Head>
+			<SplashScreen quote={quote} />
+		</div>
+	);
 }
