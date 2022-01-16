7a82107568454505696b8dbd4d0e664cd956bb88
Yog <yog.panjarale@gmail.com>
2022-01-17 00:26:28 +0530

    bug fixed

--- components/pages/SetUpProfile.tsx
+++ components/pages/SetUpProfile.tsx
@@ -50,7 +50,7 @@ export default function SetUpProfilePage() {
 }
 type InputProps = {
 	placeholder: string;
-	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	onChange: (e: React.ChangeEvent<HTMLInputElement| HTMLTextAreaElement>) => void;
 	value: string;
 	large?: boolean;
 }
@@ -58,10 +58,10 @@ function InputM({placeholder,value,onChange,large}:InputProps): JSX.Element {
 	if (large){
 		return <textarea
 		className={`bg-white text-primary-dark  text-base font-blinker font-medium py-4 px-2 text-left placeholder:text-sm active:border-solid focus:outline-none rounded-sm placeholder:text-black placeholder:font-blinker  h-min`}
-		type="text"
 		placeholder={placeholder}
 		value={value}
-		onChange={onChange} />;
+		onChange={onChange}
+		/>;
 	}
 	return (<input className="marker:bg-white text-primary-dark  text-base font-blinker font-medium py-4 px-2 text-left placeholder:text-sm active:border-solid focus:outline-none rounded-sm placeholder:text-black placeholder:font-blinker  h-min"
 		type="text"
