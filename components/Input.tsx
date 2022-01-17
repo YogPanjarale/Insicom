b574facbbf88c30b4c275892cb2607c25eaf72e3
Yog <yog.panjarale@gmail.com>
2022-01-17 20:08:22 +0530

    border now on active

--- components/Input.tsx
+++ components/Input.tsx
@@ -7,12 +7,12 @@ type InputProps = {
 export function InputM({ placeholder, value, onChange, large }: InputProps): JSX.Element {
 	if (large) {
 		return <textarea
-			className="bg-white text-primary-dark  text-base font-blinker font-medium py-4 px-2 text-left placeholder:text-sm border-2 border-dashed border-gray-400 active:border-solid  focus outline-none rounded-sm placeholder:text-black placeholder:font-blinker  h-min"
+			className="bg-white text-primary-dark  text-base font-blinker font-medium py-4 px-2 text-left placeholder:text-sm border-2 border-dashed border-gray-400 active:border-solid focus:border-solid focus outline-none rounded-sm placeholder:text-black placeholder:font-blinker  h-min"
 			placeholder={placeholder}
 			value={value}
 			onChange={onChange} />;
 	}
-	return (<input className="marker:bg-white text-primary-dark  text-base font-blinker font-medium py-4 px-2 text-left placeholder:text-sm border-2 border-dashed border-gray-400 active:border-solid focus:outline-none rounded-sm placeholder:text-black placeholder:font-blinker  h-min"
+	return (<input className="marker:bg-white text-primary-dark  text-base font-blinker font-medium py-4 px-2 text-left placeholder:text-sm border-2 border-dashed border-gray-400 active:border-solid focus:border-solid focus:outline-none rounded-sm placeholder:text-black placeholder:font-blinker  h-min"
 		type="text"
 		placeholder={placeholder}
 		value={value}
