fb5b7513090b1883de568e3d55b2f5853431cd13
Yog <yog.panjarale@gmail.com>
2022-01-18 23:26:44 +0530

    document head in head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const Document = () => (
   <Html>
+    <head>
         <title>Insicom</title>
         <link rel="icon" href="/assets/logo.svg" />
         <MetaTags title='Insicom' description="Insicom | Smarter Everyday" image_url='/assets/logo.svg' url={'https://insicom.vercel.app'}/>
         <Head/>
+    </head>
     <body>
       <Main />
       <NextScript />
