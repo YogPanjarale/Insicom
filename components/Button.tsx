e83e062c73dcdccfd910007b72aaca15c0ad6742
Yog <yog.panjarale@gmail.com>
2022-01-18 23:17:13 +0530

    Button fixed

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,7 +11,7 @@ export function Button({ text, onClick, logo }: Props) {
 			className="bg-white text-primary-dark  text-sm text-left  font-blinker font-medium py-4 pr-4 pl-2
 			  focus:border-2  border-[1px] border-solid border-black shadow-rose-100 shadow-lg	
 			 rounded-sm flex flex-row items-center space-x-4 "
-			onClick={()=>console.log("clicked")}
+			onClick={onClick}
 		>
 
 			{logo && <div className="flex items-center justify-center">{logo}</div>}
